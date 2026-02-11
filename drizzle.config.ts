ea8cd6310bb9793cee6fbfec38e84c9375be9e01
Rafi el Idrissi <rafi.elidrissi@travelcounsellors.nl>
2026-02-11 16:53:13 +0100

    data fix

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'drizzle-kit';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: '.env.local' });
 
 export default defineConfig({
     schema: './db/schema.ts',
     out: './db/migrations',
-    dialect: 'sqlite',
+    dialect: 'turso',
     dbCredentials: {
-        url: process.env.TURSO_DATABASE_URL || 'file:./local.db',
+        url: process.env.TURSO_DATABASE_URL!,
+        authToken: process.env.TURSO_AUTH_TOKEN,
     },
 });
